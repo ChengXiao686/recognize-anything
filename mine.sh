f9281ef98de0429a1ab6ccf374d163af832345c9
ChengXiao686 <xiao.cheng@autra.tech>
2023-08-01 20:28:03 +0800

    fix batch_infer_mine.py

---
+++ mine.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#############################################################################
+# Copyright 2023 Autra.Tech. All Rights Reserved.
+#############################################################################
+
+IMAGE_PATH=$1
+
+python batch_infer_mine.py \
+  --model-type ram \
+  --backbone swin_l \
+  --checkpoint pretrained/ram_swin_large_14m.pth \
+  --threshold-file ram/data/ram_tag_list_threshold.txt \
+  --image-path $IMAGE_PATH
