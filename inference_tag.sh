52d40b6c199208081981ba9856587ba363d6b9a5
ChengXiao686 <xiao.cheng@autra.tech>
2023-08-24 17:32:40 +0800

    debug

--- inference_tag.sh
+++ inference_tag.sh
@@ -10,7 +10,7 @@ python3 batch_infer_mine.py \
   --backbone swin_l \
   --checkpoint pretrained/ram_swin_large_14m.pth \
   --threshold-file ram/data/ram_tag_list_threshold.txt \
-  --save_tags true \
-  --is_prod false \
+  --save-tags true \
+  --is-prod false \
   --batch-size 64 \
   --record-path $RECORD_PATH
